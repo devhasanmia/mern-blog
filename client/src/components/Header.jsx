31042b169b80201f3396dba996425ec3fbb0af0a
devhasanmia <dev.hasanmia@gmail.com>
2024-04-29 07:29:17 +0600

    added Global Css

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,36 +1,23 @@
 import { Link } from "react-router-dom";
 import Logo from "../assets/logo.png";
+import { AiOutlineClose } from "react-icons/ai";
 
 const Header = () => {
   return (
-    <nav className="bg-gradient-to-r from-blue-500 to-purple-600 py-4">
-      <div className="container mx-auto flex items-center justify-between">
-        <Link to="/" className="flex items-center text-white">
-          <img src={Logo} alt="Navbar Logo" className="h-8 mr-2" />
-          <span className="font-bold text-xl">Your App Name</span>
+    <nav>
+      <div className="container nav__container">
+        <Link to="" className="nav__logo">
+          <img src={Logo} alt="Navbar Logo" />
         </Link>
-        <ul className="flex items-center space-x-6">
-          <li>
-            <Link to="/profile" className="text-white hover:text-yellow-400">
-              Profile
-            </Link>
-          </li>
-          <li>
-            <Link to="/create" className="text-white hover:text-yellow-400">
-              Create Post
-            </Link>
-          </li>
-          <li>
-            <Link to="/authors" className="text-white hover:text-yellow-400">
-              Authors
-            </Link>
-          </li>
-          <li>
-            <Link to="/logout" className="text-white hover:text-yellow-400">
-              Logout
-            </Link>
-          </li>
+        <ul>
+          <li><Link to="/profile">Profile</Link></li>
+          <li><Link to="/create">Create Post</Link></li>
+          <li><Link to="/authors">Authors</Link></li>
+          <li><Link to="/logout">Logout</Link></li>
         </ul>
+        <button className="nav__toggle_btn">
+          <AiOutlineClose/>
+        </button>
       </div>
     </nav>
   );
